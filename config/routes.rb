e39c64cd0ca07fefd067bc830706a58d8860342d
Joko Triono <joko.triono14@gmail.com>
2017-09-28 10:15:07 +0700

    Fix sign-in link on wizard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'user/registrations'
+  }
 
   resource :account,   only: [:show, :update]
   resource :wizards,   only: [:show, :update]
