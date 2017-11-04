99c6d4fabf1774278c274d2ca3129f427bd9c2c0
aatir <aatir.manzur@mnsu.edu>
2017-11-04 09:00:17 +0000

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,14 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address: "smtp.zoho.com",
-  domain: "zoho.com",
-  port: 587,
-  user_name: "noreply@yova.io",
-  password: "T6#vyb9r",
-  authentication: 'plain',
-  enable_starttls_auto: true
-}
+ 
+
 end
